713336532be6fa81faaa5b994a9b1b66d142ce49
Alberto <rove.albe@gmail.com>
2021-06-02 16:22:53 +0200

    finish history

--- app/src/main/java/com/example/MagicShop/model/DatabaseAccess.java
+++ app/src/main/java/com/example/MagicShop/model/DatabaseAccess.java
@@ -1,15 +1,12 @@
 package com.example.MagicShop.model;
 
-import android.util.Log;
-
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+import com.google.gson.Gson;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
@@ -36,26 +33,26 @@ public class DatabaseAccess {
     }
 
     public void shopProduct(ProductOnSale productOnSale, User user){
-        //DataSnapshot userRef = database.child("user").child(user.getId()).get().getResult();
 
         Task<DataSnapshot> productTask = database.child("product_on_sale").child(productOnSale.getId()).get();
         while (!productTask.isComplete()) {}
         DataSnapshot productRef = productTask.getResult();
-        Log.e("PRODREF",""+productRef.getValue());
         User seller = getUserFromId(productOnSale.getUser_id());
+        Date time = new Date(System.currentTimeMillis());
+        Long times = time.getTime();
 
         HistoryRecord.UserHistory hSeller = HistoryRecord.UserHistory.create(
                 seller.getUsername(), seller.getEmail(), seller.getLocation(), seller.getAddress(),seller.getCap());
 
         HistoryRecord hRecordSeller = HistoryRecord.create((Long)productRef.child("product_id").getValue(),
-                (Long)productRef.child("price").getValue(), hSeller);
+                (Long)productRef.child("price").getValue(),times , hSeller);
 
 
         HistoryRecord.UserHistory hBuyer = HistoryRecord.UserHistory.create(
                 user.getUsername(), user.getEmail(), user.getLocation(), user.getAddress(),user.getCap());
 
         HistoryRecord hRecordBuyer = HistoryRecord.create((Long)productRef.child("product_id").getValue(),
-                (Long)productRef.child("price").getValue(), hBuyer);
+                (Long)productRef.child("price").getValue(), times, hBuyer);
 
 
         database.child("user").child(user.getId()).child("history_shop").push().setValue(hRecordSeller);
@@ -65,6 +62,31 @@ public class DatabaseAccess {
         database.child("product_on_sale").child(productOnSale.getId()).removeValue();
 
     }
+
+    public List<HistoryRecord> getHistorySell(User user){
+        List<HistoryRecord> history = new ArrayList<>();
+        Task<DataSnapshot> productTask = database.child("user").child(user.getId()).child("history_sell").get();
+        while (!productTask.isComplete()) {}
+
+        for (DataSnapshot s : productTask.getResult().getChildren()) {
+            HistoryRecord record = new Gson().fromJson(s.getValue().toString(), HistoryRecord.class);
+            history.add(record);
+        }
+        return history;
+    }
+
+    public List<HistoryRecord> getHistoryShop(User user){
+        List<HistoryRecord> history = new ArrayList<>();
+        Task<DataSnapshot> productTask = database.child("user").child(user.getId()).child("history_shop").get();
+        while (!productTask.isComplete()) {}
+
+        for (DataSnapshot s : productTask.getResult().getChildren()) {
+            HistoryRecord record = new Gson().fromJson(s.getValue().toString(), HistoryRecord.class);
+            history.add(record);
+        }
+        return history;
+    }
+
 
 
     public List<ProductOnSale> getAllProductsOnSale() {
@@ -90,14 +112,12 @@ public class DatabaseAccess {
         DatabaseReference product = database.child("product");
         List<Product> prod = new ArrayList<>();
         Task<DataSnapshot> snap = product.get();
-        while (!snap.isComplete()) {
-        }
+        while (!snap.isComplete()) {}
 
         DatabaseReference language = database.child("language_" + Locale.getDefault().getLanguage());
         Task<DataSnapshot> lan = language.get();
-        while (!lan.isComplete()) {
-        }
-        ;
+        while (!lan.isComplete()) {}
+
         for (DataSnapshot s : lan.getResult().getChildren()) {
             long id = (Long) s.child("product_id").getValue();
             String name = "" + s.child("name").getValue();
@@ -116,15 +136,12 @@ public class DatabaseAccess {
     public List<ProductOnSale> getAllProductOnSaleFromProduct(Product product) {
         List<ProductOnSale> products_on_sale = new ArrayList<>();
         Task<DataSnapshot> ref_prod = database.child("product").get();
-        while (!ref_prod.isComplete()) {
-        }
+        while (!ref_prod.isComplete()) {}
 
         Task<DataSnapshot> ref_prod_on_sale = database.child("product_on_sale").get();
-        while (!ref_prod_on_sale.isComplete()) {
-        }
-        ;
-        for (DataSnapshot s : ref_prod_on_sale.getResult().getChildren()) {
+        while (!ref_prod_on_sale.isComplete()) {}
 
+        for (DataSnapshot s : ref_prod_on_sale.getResult().getChildren()) {
             if (("" + s.child("product_id").getValue()).equals("" + product.getId())) {
                 String id = s.getKey();
                 Long price = (Long) s.child("price").getValue();
@@ -350,11 +367,10 @@ public class DatabaseAccess {
     public User getUserFromId(String mId) {
         DatabaseReference users = database.child("user");
         Task<DataSnapshot> snap = users.get();
-        while (!snap.isComplete()) {
-        }
-        ;
+        while (!snap.isComplete()) {};
         for (DataSnapshot s : snap.getResult().getChildren()) {
             if (("" + s.getKey()).equals(mId)) {
+
                 String address = "" + s.child("address").getValue();
                 Long cap = (Long)s.child("cap").getValue();
                 String location = "" + s.child("location").getValue();
@@ -385,278 +401,6 @@ public class DatabaseAccess {
 
 }
 
-/*
-    public boolean logInUser(String username, String password) {
-        open();
-        boolean login=true;
-        String query = String.format("SELECT EXISTS (SELECT * FROM user WHERE username = ? AND password = ?)");
-        Cursor c = db.rawQuery(query, null);
-        Log.d("DEBUG", "query result: " + c.getCount());
-        if(c.getCount() <= 0){
-            login=false;
-        }
-        c.close();
-        close();
-        return login;
-    }
-
-
-    public User getUser(String username, String password){
-        open();
-        String query = String.format("SELECT user.username, user.email, user.city, user.address, user.cap FROM user WHERE username = ? AND password = ?");
-        Cursor c = db.rawQuery(query, null);
-        c.moveToFirst();
-        Log.println(Log.DEBUG,"username ", String.valueOf(c.isNull(0)));
-        User user = cursorToUser(c);
-        return user;
-    }
-
-
-    public List<User> getAllUsers() {
-        open();
-        List<User> users = new ArrayList<>();
-        Log.println(Log.DEBUG,"DB",db.toString());
-        Cursor c = db.rawQuery("SELECT * from user",null);
-        c.moveToFirst();
-        while (!c.isAfterLast()){
-            User p = cursorToUser(c);
-            users.add(p);
-            c.moveToNext();
-        }
-        c.close();
-        close();
-        return users;
-    }
-
-    private Product cursorToProduct(Cursor c) {
-        long id = c.getLong(0);
-        String name = c.getString(1);
-        String expansion = c.getString(2);
-        String rarity = c.getString(3);
-        String type = c.getString(4);
-        String rule = c.getString(5);
-        String img = c.getString(6);
-        return Product.create(id, name, expansion, rarity, type, rule, img);
-    }
-
-    private ProductOnSale cursorToProductOnSale(Cursor c) {
-        long id = c.getLong(0);
-        long product_id = c.getLong(1);
-        long user_id = c.getLong(2);
-        long price = c.getLong(3);
-        return ProductOnSale.create(id, product_id, user_id, price);
-    }
-
-//    private User cursorToUser(Cursor c) {
-//        String name = c.getString(0);
-//        return User.create().withUsername(name);
-//    }
-
-    //TODO necessiterà un fix
-    private User cursorToUser(Cursor c){
-        //long id = c.getLong(0);
-        String username = c.getString(0);
-        String email = c.getString(1);
-        String location = c.getString(2);
-        String address = c.getString(3);
-        long cap = c.getLong(4);
-        return User.create().withUsername(username).
-                withEmail(email).withLocation(location).withAddress(address).withCap(cap);
-    }
-
-
-    /*
-        public List<ProductOnSale> getAllProductsOnSale() {
-        open();
-        List<ProductOnSale> products_on_sale = new ArrayList<>();
-        Log.println(Log.DEBUG,"DB",db.toString());
-        Cursor c = db.rawQuery("SELECT * from product_on_sale",null);
-        c.moveToFirst();
-        while (!c.isAfterLast()){
-            ProductOnSale p = cursorToProductOnSale(c);
-            products_on_sale.add(p);
-            c.moveToNext();
-        }
-        c.close();
-        close();
-        return products_on_sale;
-    }
-
-
-
-    public List<Product> getAllProducts() {
-        open();
-        List<Product> products = new ArrayList<>();
-        Log.println(Log.DEBUG,"DB",db.toString());
-        String query = String.format("SELECT product.id, language_%1$s.name, " +
-                "language_%1$s.expansion, language_%1$s.rarity, language_%1$s.type, " +
-                "language_%1$s.rule, product.img FROM product INNER JOIN language_%1$s ON " +
-                "language_%1$s.product_id = product.id", Locale.getDefault().getLanguage());
-        Log.e("QUERY", ""+query);
-
-        Cursor c = db.rawQuery(query,null);
-        c.moveToFirst();
-        while (!c.isAfterLast()){
-            Product p = cursorToProduct(c);
-            products.add(p);
-            c.moveToNext();
-        }
-        c.close();
-        close();
-        return products;
-    }
-
-
-
-       public List<ProductOnSale> getAllProductOnSaleFromProduct(Product product){
-        open();
-        List<ProductOnSale> products_on_sale = new ArrayList<>();
-        Log.println(Log.DEBUG,"DB",db.toString());
-        String query = String.format("SELECT product_on_sale.id, product_on_sale.product_id, " +
-                "product_on_sale.user_id, product_on_sale.price FROM product INNER JOIN " +
-                "product_on_sale ON product_on_sale.product_id = product.id INNER JOIN " +
-                "language_%s ON language_%s.product_id = product.id WHERE product.id = %s",
-                Locale.getDefault().getLanguage(), Locale.getDefault().getLanguage(), product.getId());
-        Cursor c = db.rawQuery(query,null);
-        c.moveToFirst();
-        while (!c.isAfterLast()){
-            ProductOnSale p = cursorToProductOnSale(c);
-            products_on_sale.add(p);
-            c.moveToNext();
-        }
-        c.close();
-        close();
-        return products_on_sale;
-    }
-
-    //TODO necessiterà un fix
-    public User getUsersFromProductOnSale(ProductOnSale p) {
-        open();
-        List<User> users = new ArrayList<>();
-        Log.e("ID", ""+p.getId());
-        String query = String.format("SELECT user.username FROM user INNER JOIN product_on_sale ON " +
-                        "product_on_sale.user_id = user.id WHERE product_on_sale.id = %d", p.getId());
-        Cursor c = db.rawQuery(query, null);
-        c.moveToFirst();
-        User u = cursorToUser(c);
-        c.close();
-        close();
-        Log.e("nane", ""+u.getUsername());
-        return u;
-    }
-
-
-    public Product getProductFromId(String id) {
-        open();
-        Log.println(Log.DEBUG,"DB",db.toString());
-        String query = String.format("SELECT product.id, language_%1$s.name, " +
-                "language_%1$s.expansion, language_%1$s.rarity, language_%1$s.type, " +
-                "language_%1$s.rule, product.img FROM product INNER JOIN language_%1$s ON " +
-                "language_%1$s.product_id = product.id WHERE product.id = %2$s",
-                Locale.getDefault().getLanguage(), id);
-        Log.e("QUERY", ""+query);
-
-        Cursor c = db.rawQuery(query,null);
-        c.moveToFirst();
-        Product p = cursorToProduct(c);
-        c.close();
-        close();
-        return p;
-    }
-
-    public Product getProductFromProductOnSale(ProductOnSale productOnSale){
-        open();
-        String query = String.format("SELECT product.id, product.name, product.expansion, " +
-                "product.rarity, product.type, product.img FROM product INNER JOIN " +
-                "product_on_sale ON product_on_sale.product_id = product.id WHERE " +
-                "product_on_sale.id = %d", productOnSale.getId());
-        Cursor c = db.rawQuery(query,null);
-        c.moveToFirst();
-        Product product = cursorToProduct(c);
-        c.close();
-        close();
-        return product;
-    }
-
-
-    public List<ProductOnSale> getAllProductOnSaleFromUser(User user){
-        open();
-        List<ProductOnSale> products_on_sale = new ArrayList<>();
-        Log.println(Log.DEBUG,"DB",db.toString());
-        String query = String.format("SELECT product_on_sale.id, product_on_sale.product_id, " +
-                "product_on_sale.user_id, product_on_sale.price FROM product_on_sale " +
-                "INNER JOIN user ON product_on_sale.product_id = user.id " +
-                "WHERE product.id = %d", user.getId());
-        Cursor c = db.rawQuery(query,null);
-        c.moveToFirst();
-        while (!c.isAfterLast()){
-            ProductOnSale p = cursorToProdutOnSale(c);
-            products_on_sale.add(p);
-            c.moveToNext();
-        }
-        c.close();
-        close();
-        return products_on_sale;
-    }
-
-
-    public List<String> getAllExpansion(){
-        open();
-        List<String> expansions = new ArrayList<>();
-        Log.println(Log.DEBUG,"DB",db.toString());
-        String query = String.format("SELECT DISTINCT language_%1$s.expansion FROM language_%1$s",
-                Locale.getDefault().getLanguage());
-
-        Cursor c = db.rawQuery(query,null);
-        c.moveToFirst();
-        expansions.add("All");
-        while (!c.isAfterLast()){
-            expansions.add(c.getString(0));
-            c.moveToNext();
-        }
-        c.close();
-        close();
-        return expansions;
-    }
-
-
-     public List<String> getAllTypes(){
-        open();
-        List<String> types = new ArrayList<>();
-        Log.println(Log.DEBUG,"DB",db.toString());
-        String query = String.format("SELECT DISTINCT language_%1$s.type FROM language_%1$s",
-                Locale.getDefault().getLanguage());
-        Cursor c = db.rawQuery(query,null);
-        c.moveToFirst();
-        types.add("All");
-        while (!c.isAfterLast()){
-            types.add(c.getString(0));
-            c.moveToNext();
-        }
-        c.close();
-        close();
-        return types;
-    }
-
-
-    public List<String> getAllRaritys(){
-        open();
-        List<String> raritys = new ArrayList<>();
-        Log.println(Log.DEBUG,"DB",db.toString());
-        String query = String.format("SELECT DISTINCT language_%1$s.rarity FROM language_%1$s",
-                Locale.getDefault().getLanguage());
-        Cursor c = db.rawQuery(query,null);
-        c.moveToFirst();
-        raritys.add("All");
-        while (!c.isAfterLast()){
-            raritys.add(c.getString(0));
-            c.moveToNext();
-        }
-        c.close();
-        close();
-        return raritys;
-    }
-     */
 
 
 
